41b69d4aeac3ff3ee649f91d58895f674ce21807
hexaalif <hexaalif2020@gmail.com>
2023-05-13 13:42:22 +0600

    first commit

--- S_Search_In_Matrix.c
+++ S_Search_In_Matrix.c
@@ -2,6 +2,37 @@
 
 int main()
 {
+    int row, col;
+    scanf("%d %d", &row, &col);
+    int a[row][col];
 
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    int x;
+    scanf("%d", &x);
+    int flag = 1;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (a[i][j] == x)
+            {
+                flag = 0;
+            }
+        }
+    }
+    if (flag == 0)
+    {
+        printf("will not take number");
+    }
+    else
+    {
+        printf("will take number");
+    }
     return 0;
 }

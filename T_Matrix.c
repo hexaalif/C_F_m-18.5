6f696b0093f009d708a0496bdd89f8bdd32d619e
hexaalif <hexaalif2020@gmail.com>
2023-05-13 12:21:46 +0600

    first commit

--- T_Matrix.c
+++ T_Matrix.c
@@ -1,47 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    int row, col;
-    scanf("%d %d", &row, &col);
-    int a[row][col];
+    int row;
+    scanf("%d", &row);
+    int a[row][row];
     for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < col; j++)
+        for (int j = 0; j < row; j++)
         {
             scanf("%d", &a[i][j]);
         }
     }
     int flag = 1;
-    int sum = 0;
-    if (row != col)
+    long long sum = 0;
+    long long sum1 = 0;
+    long long sum2 = 0;
+    if (row != row)
     {
         flag = 0;
         printf("not dia");
     }
     for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < col; j++)
+        for (int j = 0; j < row; j++)
         {
             if (i == j)
             {
-                sum = sum + a[i][j];
+                sum1 = sum1 + a[i][j];
                 continue;
             }
-            if (a[i][j] != 0)
+            else if (i + j == row - 1)
             {
-                flag = 0;
+                sum2 = sum2 + a[i][j];
             }
+            // if (a[i][j] != 0)
+            // {
+            //     flag = 0;
+            // }
         }
     }
-    printf("%d\n", sum);
-    if (flag == 1)
-    {
-        printf("primary");
-    }
-    else
-    {
-        printf("not");
-    }
+    sum = abs(sum1) + abs(sum2);
+    printf("%lld\n", sum * 1);
     return 0;
 }
+// if (flag == 1)
+// {
+//     printf("primary");
+// }
+// else
+// {
+//     printf("not");
+// }
